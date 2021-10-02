6ccde8f8b888dc031a3e8a35df78678ecf7059cd
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-10-02 19:03:08 +0300

    repo added

---
+++ src/main/java/com/oguzcanbudumlu/reactandspringdatarest/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.oguzcanbudumlu.reactandspringdatarest.payroll;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+}
